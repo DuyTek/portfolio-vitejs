f1163ff4dc02f8c7a47c4b79c9287b1b807b9143
Duy Nguyen Dang Vu <duy.dnguyen@katalon.com>
2024-07-16 09:35:01 +0700

    Add theme

---
+++ src/AppProviders.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "@mui/material";
+import { PropsWithChildren } from "react";
+import theme from "./theme";
+
+export default function AppProviders({ children }: PropsWithChildren) {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
